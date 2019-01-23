2d01fd1cbe95f259528ff031046d311c0141cd57
muneebwasikhan <muneebwasikhan@gmail.com>
2019-01-23 17:50:58 +0500

    more work

--- App.js
+++ App.js
@@ -6,73 +6,126 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,Image,ImageBackground} from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  TextInput
+} from "react-native";
+import { Button,Input } from "react-native-elements";
+import { TextField } from 'react-native-material-textfield';
 
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
-
 
 export default class App extends Component {
   render() {
     return (
-      <ImageBackground source={require('./Assets/view.jpg')}
-      style={styles.backgroundImage}>
-
-<View style={styles.container}>
-      <View style={styles.f1}>
-      {/* <SvgUri width="200" height="200" source={require('https://www.piaic.org/static/media/Logo.fb7de414.svg')} /> */}
-      <Image
-          style={{width: 150, height: 75}}
-          source={{uri: 'https://www.panacloud.ai/assets/images/global/Logo.png'}}
+      <ImageBackground
+        source={require("./Assets/view.jpg")}
+        style={styles.backgroundImage}
+      >
+        <View style={styles.container}>
+          <View style={styles.f1}>
+            {/* <SvgUri width="200" height="200" source={require('https://www.piaic.org/static/media/Logo.fb7de414.svg')} /> */}
+            <Image
+              style={{ width: 150, height: 75 }}
+              source={{
+                uri: "https://www.panacloud.ai/assets/images/global/Logo.png"
+              }}
+            />
+          </View>
+          <View style={styles.f2}>
+            {/* <Button titleStyle={{fontSize: 15}} buttonStyle={styles.loginBtn} title="LOGIN" /> */}
+            <View style={styles.loginSection}>
+            <TextInput
+          style={styles.inputFiledLogin}
+          placeholder="Type here to translate!"
+          placeholderTextColor="white"
+          onChangeText={(text) => this.setState({text})}
         />
-    </View>
-      <View style={styles.f2}><Text>g bhaiii</Text></View>
-</View>
-</ImageBackground>
+         <TextField
+        label='Phone number'
+        value="gfgff"
+      />
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  inputFiledLogin:{
+    width: '80%',
+    // backgroundColor: 'white',
+    height: 40,
+    marginLeft: '10%',
+    borderTopColor: 'white',
+    borderTopWidth: 1,
+    borderBottomColor: 'white',
+    borderBottomWidth: 1,
+    color: 'white',
+    fontSize: 15,
+    
+
+  },
+  loginSection:{
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#101b11',
+    opacity: 0.9,
+  },
+  loginBtn:{
+    backgroundColor: "#101b11",
+    height: 70,
+    width: "100%",
+    borderRadius: 0,
+
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   f1: {
     height: "50%",
-    width: '100%',
+    width: "100%",
     // backgroundColor: "blue",
-    display:"flex",
-    justifyContent: 'center',
-    alignItems: 'center',
-    
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
   },
   f2: {
     height: "50%",
-    width: '100%',
+    width: "100%",
+    justifyContent: "flex-end"
     // backgroundColor: "red"
   },
   backgroundImage: {
     flex: 1,
     width: null,
     height: null,
-    resizeMode: 'cover'
-},
+    resizeMode: "cover"
+  },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
